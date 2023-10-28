4b9fcdde25607d104770bfcc75d744df46ec44bb
htavakkoli1368 <hosseinjealous@gmail.com>
2023-10-28 12:04:11 -0700

    next commit rendering

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
-
-const userspage = () => {
+interface User{
+  id : number;
+  name:string;
+}
+const userspage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users",
+  {next:{revalidate:10}});
+  const userList:User[] = await res.json();
   return (
-    <div>userspage</div>
+    <>
+      <h1>Users</h1>
+      <ul>
+         {userList.map(user=>
+          <li key={user.id}>{user.name}</li>
+         )}
+      </ul>
+    </>
   )
 }
 
