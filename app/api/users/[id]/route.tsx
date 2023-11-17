c2a10ebede943a8c2ea55415ff192b456eb3088e
htavakkoli1368 <hosseinjealous@gmail.com>
2023-11-17 02:09:10 -0800

    add zod library in order to validation our schema

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-
+import schema from  "../schema"
 export function GET(request:NextRequest,{params}:{params:{id:number}}){
    if(params.id>10){
     return NextResponse.json(      
@@ -16,8 +16,9 @@ export function GET(request:NextRequest,{params}:{params:{id:number}}){
 }
 export async function PUT(request:NextRequest,{params}:{params:{id:number}}){
    const body = await request.json();
-   if (!body.name) {
-      return NextResponse.json({error:"invalid name"},{status:400});
+   const validation =schema.safeParse(body);
+   if (!validation.success) {
+      return NextResponse.json(validation.error.errors,{status:400});
    }
    if (params.id>10) {
       return NextResponse.json({error:"user not found"},{status:400});
--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import schema from "./schema"
 
 export function GET(request:NextRequest){
    return NextResponse.json(
@@ -10,8 +11,9 @@ export function GET(request:NextRequest){
 }
 export async function POST(request:NextRequest){
    const body = await request.json();
-   if (!body.name) {
-      return NextResponse.json({error:"invalid"},{status:485});
+   const validation = schema.safeParse(body);
+   if (!validation.success) {
+      return NextResponse.json(validation.error.errors,{status:485});
    }
    else{
       return NextResponse.json({id:1,name:body.name});       
